dd4e33ed99451b5ba2909ad4ac26af25395137a1
RifatArefin32 <arefinrifat786@gmail.com>
2024-06-04 11:30:17 +0600

    04062024 1130

--- modules/product/controllers/ProductController.php
+++ modules/product/controllers/ProductController.php
@@ -9,4 +9,66 @@ use yii\rest\ActiveController;
  */
 class ProductController extends ActiveController{
     public $modelClass = \app\models\Product::class;
+
+    public function behaviors(){
+        $behaviors = parent::behaviors();
+        $behaviors["authenticator"] = [
+            
+            // Bearar Token Authentication
+            // "class" => \yii\filters\auth\HttpBearerAuth::class,
+
+            // Basic HTTP Authentication
+            // "class"=> \yii\filters\auth\HttpBasicAuth::class,
+            
+            // QueryParams Authentication
+            // "class"=> \yii\filters\auth\QueryParamAuth::class,
+            // "tokenParam"=> 'API_KEY'
+
+            // Composite Authentication
+            // "class"=> \yii\filters\auth\CompositeAuth::class,
+            // "authMethods"=> [
+            //     \yii\filters\auth\HttpBasicAuth::class,
+            //     //\yii\filters\auth\HttpBearerAuth::class,
+            //     [
+            //         "class"=> \yii\filters\auth\QueryParamAuth::class,
+            //         "tokenParam"=> 'mahim',
+            //     ]
+            // ]
+
+            // Exclude endpoint authentication using Bearar Token Authentication
+            // "class" => \yii\filters\auth\HttpBearerAuth::class,
+            // "except"=> ['test'],
+
+            // Optional endpoint authentication using Bearar Token Authentication
+            // "class" => \yii\filters\auth\HttpBearerAuth::class,
+            // "optional" => ['optional-test']
+
+            // Specific action authentication using Bearar Token Authentication
+            // "class" => \yii\filters\auth\HttpBearerAuth::class,
+            // "only"=> ['test']
+
+            // "class"=> \yii\filters\auth\CompositeAuth::className(),
+            // "authMethods"=> [
+            //     [
+            //         "class"=> \yii\filters\auth\HttpBasicAuth::className(),
+            //         "only"=> ['test'],
+            //     ],
+            //     [
+            //         'class'=> \yii\filters\auth\QueryParamAuth::className(),
+            //         'tokenParam' => 'mahim',
+            //         'only' => ['optional-test'],
+            //     ]
+            // ],
+        ];
+        return $behaviors;
+    }
+
+
+    public function actionTest(){
+        return ['message' => 'Hi Test Action'];
+    }
+
+    public function actionOptionalTest(){
+        return ['message' => 'Hi Optional Action'];
+    }
 }
